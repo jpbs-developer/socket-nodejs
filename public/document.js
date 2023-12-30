ad4d5c3182fa707892b69c611c5c87f4a183b60e
jpbs-developer <jpbs1@aluno.ifal.edu.br>
2023-12-30 00:16:05 -0300

    socket rooms

--- public/document.js
+++ public/document.js
@@ -1,12 +1,24 @@
 const socket = io();
 const input = document.querySelector("#editor-texto");
+const documentTitle = document.querySelector("#titulo-documento");
+
+const params = new URLSearchParams(window.location.search);
+const pageTitle = params.get("nome");
+
+documentTitle.textContent = pageTitle || "Documento sem título";
+
+socket.emit("enter", pageTitle, (value) => {
+    input.value = value;
+})
 
 input.addEventListener("keyup", (event) => {
   const value = event.target.value;
   if (value) {
-    socket.emit("send", value);
+    socket.emit("send", value, pageTitle);
   }
 });
 socket.on("send:receive", (value) => {
   input.value = value;
 });
+
+
--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,44 @@
 import io from "./server.js";
 
+const documents = [
+  {
+    name: "Javascript",
+    text: "javascript",
+  },
+  {
+    name: "Nodejs",
+    text: "nodejs",
+  },
+  {
+    name: "Socket.io",
+    text: "socket.io",
+  },
+];
+
 io.on("connection", (socket) => {
-  console.log("Connection established");
-  socket.on("send", (value) => {
-    socket.broadcast.emit("send:receive", value);
+  socket.on("enter", (document, callBack) => {
+    socket.join(document);
+
+    const matchDocument = getDocument(document);
+
+    if (matchDocument) {
+      callBack(matchDocument.text);
+    }
+  });
+  socket.on("send", (value, document) => {
+    const matchDocument = getDocument(document);
+    if (matchDocument) {
+      matchDocument.text = value;
+      socket.to(document).emit("send:receive", value);
+    }
+
+    console.log(matchDocument);
   });
 });
+
+const getDocument = (name) => {
+  const document = documents.find(
+    (doc) => doc.name.toLowerCase() === name.toLowerCase()
+  );
+  return document;
+};
